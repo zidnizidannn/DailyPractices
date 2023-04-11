2984780bc8f202df0f5e0c1ac56fcde65046903d
Zidni Zidan Mahestra Setyawan <zidnizidandmc@gmail.com>
2023-04-12 06:54:08 +0700

    selesai Menambahkan gambar pada dadu

--- app/src/main/java/com/cermat/myapplication/MainActivity.kt
+++ app/src/main/java/com/cermat/myapplication/MainActivity.kt
@@ -2,7 +2,7 @@ package com.cermat.myapplication
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +16,18 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
